fc2f808fb77fd66975ea56722f2375ec6fefce51
Lordet-Iron <harrison.lockwood@mail.bcu.ac.uk>
2025-03-14 12:22:58 +0000

    made rts controls with basic click detection

---
+++ Assets/RTSPlayerControls.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RTSPlayerControls : MonoBehaviour
+{
+    [SerializeField] private CameraMovement cameraMovement;
+    private Vector2 screenPosition;
+    
+
+    /// <summary>
+    /// This is called all the time to aquire screen position and update the screenPosition variable
+    /// </summary>
+    /// <param name="context"></param>
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        Vector2 screenPosition = context.ReadValue<Vector2>();
+    }
+
+    /// <summary>
+    /// Called whenever the player presses their mouse
+    /// </summary>
+    /// <param name="context"></param>
+    public void Click(InputAction.CallbackContext context)
+    {
+        float clickValue = context.ReadValue<float>();
+
+        if (clickValue > 0) // Button pressed
+        {
+            OnClickStarted();
+        }
+        else // Button released
+        {
+            OnClickEnded();
+        }
+    }
+    private void OnClickStarted()
+    {
+        Debug.Log("Started");
+    }
+
+    private void OnClickEnded()
+    {
+        Debug.Log("Ended");
+    }
+
+}
