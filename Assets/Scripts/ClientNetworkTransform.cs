270f5fe20182577b0989eb965b1ec41af4d6f745
TylerAshton <tyler.ashton-linnett@mail.bcu.ac.uk>
2025-03-27 13:45:02 +0000

    Cleaned up code

--- Assets/Scripts/ClientNetworkTransform.cs
+++ Assets/Scripts/ClientNetworkTransform.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using Unity.Netcode.Components;
 
+/// <summary>
+/// Changes the network transform to be Client Authorative instead of Server (DO NOT USE)
+/// </summary>
 public class ClientNetworkTransform : NetworkTransform
 {
     protected override bool OnIsServerAuthoritative()
--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -2,22 +2,32 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Static loader to load a given scene
+/// </summary>
 public static class Loader
 {
-    public enum Scene
+    public enum Scene //An enum of the different scenes that the network can load between
     {
         MainMenu,
         LoadingScene,
         MainWorld
     }
 
-    private static Scene targetScene;
+    private static Scene targetScene; //The scene the game will transition to
 
+    /// <summary>
+    /// Loads the scene across the network
+    /// </summary>
+    /// <param name="targetScene"></param>
     public static void LoadNetwork(Scene targetScene)
     {
         NetworkManager.Singleton.SceneManager.LoadScene(targetScene.ToString(), LoadSceneMode.Single);
     }
 
+    /// <summary>
+    /// Loads the scene locally for whoever calls it
+    /// </summary>
     public static void LoaderCallback()
     {
         SceneManager.LoadScene(targetScene.ToString());
