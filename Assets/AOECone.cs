f40f6207f615eeffff1b1db5115cc1a9bf9b73c9
LordSiron <149695698+Lordet-Iron@users.noreply.github.com>
2025-04-09 13:02:22 +0100

    made demon character

--- Assets/AOECone.cs
+++ Assets/AOECone.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using UnityEngine;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 public class AOECone : MonoBehaviour
 {
     [SerializeField] float lifeTimeSec = 2f;
+    [SerializeField] float angleDegrees = 90f;
+    [SerializeField] float range = 4f;
+    [SerializeField] float damage = 1f;
 
     private void Start()
     {
@@ -14,5 +18,33 @@ public class AOECone : MonoBehaviour
     {
         yield return new WaitForSeconds(lifeTimeSec);
         Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+
+    }
+
+    private void ApplyDamage()
+    {
+        Vector3 origin = transform.position;
+        Vector3 forward = transform.forward;
+        float cosAngle = Mathf.Cos(angleDegrees * 0.5f * Mathf.Deg2Rad); // Conversion of our angleDegrees to a cos for dot. 
+                                                                         // We use half as the full cone is angleDegrees
+
+        Collider[] hits = Physics.OverlapSphere(origin, range);
+        foreach (var hit in hits)
+        {
+            Vector3 toTarget = (hit.transform.position - origin).normalized;
+            float dot = Vector3.Dot(forward, toTarget);
+
+            if (dot >= cosAngle) // If is within our cone degrees, hit
+            {
+                if (hit.TryGetComponent(out Health _health))
+                {
+                    _health.Damage(damage * Time.deltaTime);
+                }
+            }
+        }
     }
 }
