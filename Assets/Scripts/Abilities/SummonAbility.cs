2f22e142650a4b5ad5409d1d200ef8ea412bba1d
Lordet-Iron <harrison.lockwood@mail.bcu.ac.uk>
2025-04-23 14:21:38 +0100

    Aquired assets

---
+++ Assets/Scripts/Abilities/SummonAbility.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonAbility : Ability
+{
+    public override void Activate(GameObject user, Animator _animator)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnUse(GameObject _user, List<Transform> _abilityPositions)
+    {
+        throw new System.NotImplementedException();
+    }
+}
