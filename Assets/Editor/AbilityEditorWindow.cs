e0d4fcfcdf3e6d3aa25c909ec1ef8e0523e149b3
Lordet-Iron <harrison.lockwood@mail.bcu.ac.uk>
2025-06-17 17:06:51 +0100

    moved inspector drawing to the ability class

--- Assets/Editor/AbilityEditorWindow.cs
+++ Assets/Editor/AbilityEditorWindow.cs
@@ -26,43 +26,9 @@ namespace Editor
 
             so.Update();
 
-            // Custom fields
-            SerializedProperty fieldID = so.FindProperty("abilityID");
-            fieldID.stringValue = EditorGUILayout.TextField("ID", fieldID.stringValue);
-            if (fieldID.stringValue == "")
-            {
-                EditorGUILayout.HelpBox("Ability ID Can't be null", MessageType.Error);
-            }
-
-            SerializedProperty fieldName = so.FindProperty("abilityName");
-            fieldName.stringValue = EditorGUILayout.TextField("Name", fieldName.stringValue);
-
-            SerializedProperty fieldAnimationTrigger = so.FindProperty("animationTrigger");
-            fieldAnimationTrigger.stringValue = EditorGUILayout.TextField("Animation Trigger", fieldAnimationTrigger.stringValue); // TODO: Remove?
-            //EditorGUILayout.HelpBox("Honestly don't animationTrigger touch this without a dev.", MessageType.Warning);
-
-            SerializedProperty fieldCastTime = so.FindProperty("castTime");
-            fieldCastTime.floatValue = EditorGUILayout.Slider("Cast Time", fieldCastTime.floatValue, 0, 10);
-
-            SerializedProperty fieldCooldown = so.FindProperty("cooldown");
-            fieldCooldown.floatValue = EditorGUILayout.Slider("Cooldown", fieldCooldown.floatValue, 0, 60);
-
-            SerializedProperty fieldAbilityCost = so.FindProperty("abilityCost");
-            fieldAbilityCost.intValue = EditorGUILayout.IntField("Ability Cost", fieldAbilityCost.intValue);
-
-            if (fieldAbilityCost.intValue < 0)
-            {
-                EditorGUILayout.HelpBox("Ability Cost cannot be negative.", MessageType.Error);
-            }
+            // Draw ability fields
+            selectedAbility.DrawInspector(so);
             
-
-
-            SerializedProperty fieldCastPos = so.FindProperty("castPositionName");
-            fieldCastPos.enumValueIndex = EditorGUILayout.Popup("Cast Position", fieldCastPos.enumValueIndex, fieldCastPos.enumDisplayNames);
-
-            SerializedProperty fieldIcon = so.FindProperty("icon");
-            fieldIcon.objectReferenceValue = EditorGUILayout.ObjectField("Ability Icon", fieldIcon.objectReferenceValue, typeof(Sprite), allowSceneObjects: false);
-
             so.ApplyModifiedProperties();
 
         }
