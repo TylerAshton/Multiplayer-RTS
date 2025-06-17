311d2d248f7bbfdb43bc892650c981cbbed9b7b9
Lordet-Iron <harrison.lockwood@mail.bcu.ac.uk>
2025-06-17 13:41:49 +0100

    error atempting to add naming

--- Assets/Editor/PropertyDrawerHI.cs
+++ Assets/Editor/PropertyDrawerHI.cs
@@ -25,13 +25,20 @@ namespace Editor
                 GenericMenu menu = new GenericMenu();
 
                 // Add ability types here - replace with your own subclasses
-                menu.AddItem(new GUIContent("Channelled Projection Ability"), false, () => CreateNewAbilityOfType($"{typeof(ChannelledProjection).FullName}", property));
+                /*menu.AddItem(new GUIContent("Channelled Projection Ability"), false, () => CreateNewAbilityOfType($"{typeof(ChannelledProjection).FullName}", property));
                 menu.AddItem(new GUIContent("Buff Ability"), false, () => CreateNewAbilityOfType($"{typeof(BuffAbility).FullName}", property));
                 menu.AddItem(new GUIContent("Construct Ability"), false, () => CreateNewAbilityOfType($"{typeof(Construct).FullName}", property));
                 menu.AddItem(new GUIContent("Melee Ability"), false, () => CreateNewAbilityOfType($"{typeof(MeleeAbility).FullName}", property));
                 menu.AddItem(new GUIContent("Projectile Ability"), false, () => CreateNewAbilityOfType($"{typeof(ProjectileAbility).FullName}", property));
                 menu.AddItem(new GUIContent("Summon Ability"), false, () => CreateNewAbilityOfType($"{typeof(SummonAbility).FullName}", property));
-                menu.AddItem(new GUIContent("DEBUG Ability"), false, () => CreateNewAbilityOfType($"{typeof(DebugAbility).FullName}", property));
+                menu.AddItem(new GUIContent("DEBUG Ability"), false, () => CreateNewAbilityOfType($"{typeof(DebugAbility).FullName}", property));*/
+                menu.AddItem(new GUIContent("Channelled Projection Ability"), false, () => ShowNamingWindow($"{typeof(ChannelledProjection).FullName}", property));
+                menu.AddItem(new GUIContent("Buff Ability"), false, () => ShowNamingWindow($"{typeof(BuffAbility).FullName}", property));
+                menu.AddItem(new GUIContent("Construct Ability"), false, () => ShowNamingWindow($"{typeof(Construct).FullName}", property));
+                menu.AddItem(new GUIContent("Melee Ability"), false, () => ShowNamingWindow($"{typeof(MeleeAbility).FullName}", property));
+                menu.AddItem(new GUIContent("Projectile Ability"), false, () => ShowNamingWindow($"{typeof(ProjectileAbility).FullName}", property));
+                menu.AddItem(new GUIContent("Summon Ability"), false, () => ShowNamingWindow($"{typeof(SummonAbility).FullName}", property));
+                menu.AddItem(new GUIContent("DEBUG Ability"), false, () => ShowNamingWindow($"{typeof(DebugAbility).FullName}", property));
 
                 menu.ShowAsContext();
             }
@@ -39,25 +46,40 @@ namespace Editor
             EditorGUI.EndProperty();
         }
 
-        private void CreateNewAbilityOfType(string abilityTypeName, SerializedProperty property)
+        private void ShowNamingWindow(string _abilityTypeName, SerializedProperty property)
+        {
+            Type abilityType = GetAbilityType(_abilityTypeName);
+
+            UnityEditor.PopupWindow.Show(
+                new Rect(GUIUtility.GUIToScreenPoint(Event.current.mousePosition), Vector2.zero),
+                new AbilityNamePopup(CreateNewAbilityOfType, abilityType, property)
+            );
+        }
+
+        /// <summary>
+        /// Creates a new ability of the specified type and assigns it to the property.
+        /// </summary>
+        /// <param name="abilityTypeName"></param>
+        /// <param name="property"></param>
+        private void CreateNewAbilityOfType(string abilityTypeName, Type _abilityType, SerializedProperty property)
         {
             // Calculating path for new ability asseet
             string folderPath = "Assets/Resources/Abilities";
             string assetName = $"New {abilityTypeName}.asset";
             string fullPath = $"{folderPath}/{assetName}";
 
-            // Create instance by type name
+/*            // Create instance by type name
             var type = GetAbilityType(abilityTypeName);
 
             if (type == null)
             {
                 Debug.LogError("abilityTypeName wasn't valid");
                 return;
-            }
+            }*/
 
             // Create asset
 
-            ScriptableObject newAbility = ScriptableObject.CreateInstance(type);
+            ScriptableObject newAbility = ScriptableObject.CreateInstance(_abilityType);
 
             // Save the asset to the specified path
             AssetDatabase.CreateAsset(newAbility, fullPath);
