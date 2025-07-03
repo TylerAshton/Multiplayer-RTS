fa3bec9bc282df48d9b7b9d7eef71edc3eb9a611
TylerAshton <tyler.ashton-linnett@mail.bcu.ac.uk>
2025-07-03 19:54:50 +0100

    Brb

--- Assets/Temp Storage Of Lost Work/MinimapHandler.cs
+++ Assets/Temp Storage Of Lost Work/MinimapHandler.cs
@@ -1,12 +1,12 @@
-using NUnit.Framework;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.Collections;
+using Unity.Netcode;
 using UnityEngine;
 
 public class MinimapHandler : MonoBehaviour
 {
     private List<GameObject> units = new List<GameObject>();
+    private Dictionary<GameObject, GameObject> UnitToIcon = new Dictionary<GameObject, GameObject>();
     [SerializeField] List<GameObject> icons;
 
     private void Start()
@@ -16,16 +16,70 @@ public class MinimapHandler : MonoBehaviour
 
     private void updateList()
     {
+        deleteIcons();
         units.Clear();
         units = LayerList.FindGameObjectsWithLayer("Unit");
+        createIcon();
+    }
+
+    private void createIcon()
+    {
+        foreach (GameObject unit in units)
+        {
+            if (unit.CompareTag("Champion"))
+            {
+                if (unit.GetComponent<NetworkObject>().IsOwner)
+                {
+                    GameObject icon = (GameObject)Instantiate(Resources.Load("Icons/Main Champion Icon"), unit.transform);
+                    icons.Add(icon);
+                    UnitToIcon.Add(unit, icon);
+                }
+                else
+                {
+                    GameObject icon = (GameObject)Instantiate(Resources.Load("Icons/Sub Champion Icon"), unit.transform);
+                    icons.Add(icon);
+                    UnitToIcon.Add(unit, icon);
+                }
+            }
+            else if (unit.CompareTag("Amalgam"))
+            {
+                GameObject icon = (GameObject)Instantiate(Resources.Load("Icons/Main Champion Icon"), unit.transform);
+                icons.Add(icon);
+                UnitToIcon.Add(unit, icon);
+            }
+            else
+            {
+                Debug.LogError($"UNIT WITHOUT CORRECT TAG : {unit.name}");
+            }
+        }
+    }
+
+    private void deleteIcons()
+    {
+        foreach(GameObject unit in units)
+        {
+            try
+            {
+                Destroy(UnitToIcon[unit]);
+            }
+            catch (NullReferenceException)
+            {
+                Debug.LogError("Attempted to kill gameobject that doesnt exist :l");
+            }
+        }
     }
 
     private void moveIcons()
     {
-        updateList();
         foreach (GameObject unit in units)
         {
-            
+            if (unit.CompareTag("Champion"))
+            {
+                if (unit.GetComponent<NetworkObject>().IsOwner)
+                {
+
+                }
+            }
         }
     }
 }
