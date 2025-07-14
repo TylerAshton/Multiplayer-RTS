e5c8a88819012ba415739db8b50118af74b37dfa
Lordet-Iron <harrison.lockwood@mail.bcu.ac.uk>
2025-07-14 12:24:39 +0100

    Removed old registries

--- Assets/Scripts/RegistryManager.cs
+++ Assets/Scripts/RegistryManager.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 
+/// <summary>
+/// Loads and initialises the Registries
+/// </summary>
 public static class RegistryManager
 {
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Init()
     {
         Registry<Ability>.Init("Abilities");
-        Registry<Purchasable>.Init("AbilityStats");
+        Registry<Purchasable>.Init("");
         Registry<HitboxStats>.Init("AbilityStats");
         Registry<ProjectileStats>.Init("AbilityStats");
         Registry<ProjectionStats>.Init("AbilityStats");
