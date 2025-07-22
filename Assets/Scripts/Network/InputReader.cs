a13cc16a701088bad679d417d2a58483924e7c5d
TylerAshton <tyler.ashton-linnett@mail.bcu.ac.uk>
2025-07-22 17:54:12 +0100

    Rotation and Animations are broken

--- Assets/Scripts/Network/InputReader.cs
+++ Assets/Scripts/Network/InputReader.cs
@@ -2,18 +2,43 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [CreateAssetMenu(fileName = "Input Reader", menuName = "Player/Input Reader")]
-public class InputReader : ScriptableObject, PlayerInputActions.IPlayerActions
+public class InputReader : ScriptableObject, InputSystem_Actions.IPlayerActions
 {
     public Vector2 Move => inputActions.Player.Move.ReadValue<Vector2>(); // Creates a public Vector2 to hold the input values of the players movement input
+    public Vector2 Point => inputActions.Player.Point.ReadValue<Vector2>(); // Creates a public Vector2 to hold the input values of the players pointer
 
-    PlayerInputActions inputActions; // Reference to the players input action. This holds all the binds that the user will implement
+    InputSystem_Actions inputActions; // Reference to the players input action. This holds all the binds that the user will implement
 
     void OnEnable()
     {
         if (inputActions == null)
         {
-            inputActions = new PlayerInputActions();
+            inputActions = new InputSystem_Actions();
             inputActions.Player.SetCallbacks(this);
+        }
+    }
+
+    public void AutoToggle()
+    {
+        if (inputActions.Player.enabled)
+        {
+            inputActions.Disable();
+        }
+        else
+        {
+            inputActions.Enable();
+        }
+    }
+
+    public void ManualToggle(bool _state)
+    {
+        if (_state)
+        {
+            inputActions.Enable();
+        }
+        else
+        {
+            inputActions.Disable();
         }
     }
 
@@ -43,5 +68,41 @@ public class InputReader : ScriptableObject, PlayerInputActions.IPlayerActions
     public void OnShop(InputAction.CallbackContext context)
     {
         
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnPrevious(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnNext(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnDebugRevive(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
     }
 }
