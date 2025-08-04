f334ceab61bbdd8da6666eecc8733dc8693eeef9
Tyler Ashton-Linnett <tyler.ashton-linnett@mail.bcu.ac.uk>
2025-08-04 16:17:55 +0100

    Set materials to GPU Instancing Mode

---
+++ Assets/Scripts/Scriptable Fmod Example.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ScriptableFmodExample : MonoBehaviour
+{
+    [SerializeField] private FMODUnity.EventReference FmodEvent;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
